505506a05ecc66b24a74b8d23fd96bc7e14decd5
Stepan Bujnak <stepan.bujnak@gmail.com>
2023-11-09 22:53:50 +0100

    Added private search support

---
+++ lib/PrivateSearchClient.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Pex;
+
+class PrivateSearchClient extends BaseClient
+{
+    public function __construct(string $clientID, string $clientSecret)
+    {
+        parent::__construct(Lib::get()->Pex_PRIVATE_SEARCH, $clientID, $clientSecret);
+    }
+
+    public function startSearch(PrivateSearchRequest $req): SearchFuture
+    {
+        return $this->internalStartSearch($req->getFingerprint());
+    }
+
+    public function ingest(string $providedID, Fingerprint $ft): void
+    {
+        $defer = new Defer();
+
+        Lib::get()->Pex_Lock();
+        $defer->add(Lib::get()->Pex_Unlock);
+
+        $buffer = Lib::get()->Pex_Buffer_New();
+        Error::checkMemory($buffer);
+        $defer->add(fn () => Lib::get()->Pex_Buffer_Delete(\FFI::addr($buffer)));
+
+        $status = Lib::get()->Pex_Status_New();
+        Error::checkMemory($status);
+        $defer->add(fn () => Lib::get()->Pex_Status_Delete(\FFI::addr($status)));
+
+        Lib::get()->Pex_Buffer_Set($buffer, $ft->getBytes(), strlen($ft->getBytes()));
+
+        Lib::get()->Pex_Ingest($this->client, $providedID, $buffer, $status);
+        Error::checkStatus($status);
+    }
+}
