13a10a7eb6c1c3a88177bc852cdfc212e67237e4
Stepan Bujnak <stepan.bujnak@gmail.com>
2025-07-27 09:21:40 +0200

    added content classification

--- lib/FingerprintType.php
+++ lib/FingerprintType.php
@@ -4,8 +4,9 @@ namespace Pex;
 
 class FingerprintType
 {
-    public const Video  = 1;
-    public const Audio  = 2;
+    public const Video = 1;
+    public const Audio = 2;
     public const Melody = 4;
     public const Phonetic = 8;
+    public const Class = 16;
 }
